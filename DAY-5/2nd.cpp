bcb4d56a1871b7860da1bc94838b4699867bed28
Abhishek Singh <Abhishek23main@gmail.com>
2024-09-23 15:50:18 +0530

    2nd.cpp

--- DAY-5/2nd.cpp
+++ DAY-5/2nd.cpp
@@ -1,4 +1,5 @@
 //! 2. write a cpp program that will create a class which will calculate the area of triangle, circle and rectangle having the same name.
+
 #include <iostream>
 #include <cmath>
 using namespace std;
