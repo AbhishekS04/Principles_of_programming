2eccb5b88e6905808134e5d90cc1a080be121dc5
Abhishek Singh <Abhishek23main@gmail.com>
2024-11-08 10:44:46 +0530

    Update 1st.cpp

--- DAY-6/1st.cpp
+++ DAY-6/1st.cpp
@@ -1,1 +1,83 @@
-//! write a c++ prog to impliment constructor overloding with real life senario 1.default constructor 2.paramiterised constructor 3.copy constructor 4.constructor with default argument 5.destructor
+//! write a c++ prog to impliment multi level inheritance of student registeration system ?
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Base class
+class Person
+{
+protected:
+    string name;
+    int age;
+
+public:
+    void setPersonDetails(const string &name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+
+    void displayPersonDetails() const
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+// Derived class
+class Student : public Person
+{
+protected:
+    string rollNumber;
+    string course;
+
+public:
+    void setStudentDetails(const string &rollNumber, const string &course)
+    {
+        this->rollNumber = rollNumber;
+        this->course = course;
+    }
+
+    void displayStudentDetails() const
+    {
+        displayPersonDetails();
+        cout << "Roll Number: " << rollNumber << endl;
+        cout << "Course: " << course << endl;
+    }
+};
+
+// Further derived class
+class RegisteredStudent : public Student
+{
+private:
+    string registrationNumber;
+    string registrationDate;
+
+public:
+    void setRegistrationDetails(const string &registrationNumber, const string &registrationDate)
+    {
+        this->registrationNumber = registrationNumber;
+        this->registrationDate = registrationDate;
+    }
+
+    void displayRegistrationDetails() const
+    {
+        displayStudentDetails();
+        cout << "Registration Number: " << registrationNumber << endl;
+        cout << "Registration Date: " << registrationDate << endl;
+    }
+};
+
+int main()
+{
+    RegisteredStudent student;
+    student.setPersonDetails("Abhishek singh", 20);
+    student.setStudentDetails("11", "Computer Science");
+    student.setRegistrationDetails("0008918", "01-09-2023");
+
+    student.displayRegistrationDetails();
+
+    return 0;
+}
