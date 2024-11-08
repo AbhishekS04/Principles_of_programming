765c4527e3b1b3262992620fd0d65b07de6f0d21
Abhishek Singh <Abhishek23main@gmail.com>
2024-11-08 10:48:09 +0530

    Add files via upload

--- DAY-6/1st.cpp
+++ DAY-6/1st.cpp
@@ -1,83 +1,67 @@
-//! write a c++ prog to impliment multi level inheritance of student registeration system ?
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 // Base class
-class Person
-{
+class Person {
 protected:
     string name;
     int age;
 
 public:
-    void setPersonDetails(const string &name, int age)
-    {
-        this->name = name;
-        this->age = age;
+    void setPersonDetails(string n, int a) {
+        name = n;
+        age = a;
     }
 
-    void displayPersonDetails() const
-    {
+    void displayPersonDetails() {
         cout << "Name: " << name << endl;
         cout << "Age: " << age << endl;
     }
 };
 
-// Derived class
-class Student : public Person
-{
+// Derived class from Person
+class Student : public Person {
 protected:
-    string rollNumber;
-    string course;
+    int studentID;
 
 public:
-    void setStudentDetails(const string &rollNumber, const string &course)
-    {
-        this->rollNumber = rollNumber;
-        this->course = course;
+    void setStudentDetails(string n, int a, int id) {
+        setPersonDetails(n, a);
+        studentID = id;
     }
 
-    void displayStudentDetails() const
-    {
+    void displayStudentDetails() {
         displayPersonDetails();
-        cout << "Roll Number: " << rollNumber << endl;
-        cout << "Course: " << course << endl;
+        cout << "Student ID: " << studentID << endl;
     }
 };
 
-// Further derived class
-class RegisteredStudent : public Student
-{
+// Derived class from Student
+class Registration : public Student {
 private:
-    string registrationNumber;
+    string course;
     string registrationDate;
 
 public:
-    void setRegistrationDetails(const string &registrationNumber, const string &registrationDate)
-    {
-        this->registrationNumber = registrationNumber;
-        this->registrationDate = registrationDate;
+    void setRegistrationDetails(string n, int a, int id, string c, string rDate) {
+        setStudentDetails(n, a, id);
+        course = c;
+        registrationDate = rDate;
     }
 
-    void displayRegistrationDetails() const
-    {
+    void displayRegistrationDetails() {
         displayStudentDetails();
-        cout << "Registration Number: " << registrationNumber << endl;
+        cout << "Course: " << course << endl;
         cout << "Registration Date: " << registrationDate << endl;
     }
 };
 
-int main()
-{
-    RegisteredStudent student;
-    student.setPersonDetails("Abhishek singh", 20);
-    student.setStudentDetails("11", "Computer Science");
-    student.setRegistrationDetails("0008918", "01-09-2023");
-
-    student.displayRegistrationDetails();
+int main() {
+    Registration reg;
+    reg.setRegistrationDetails("John Doe", 20, 12345, "Computer Science", "2023-10-01");
+    reg.displayRegistrationDetails();
 
     return 0;
 }
---
+++ DAY-6/3rd.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Complex {
+private:
+    float real;
+    float imag;
+
+public:
+    Complex() : real(0), imag(0) {}
+    Complex(float r, float i) : real(r), imag(i) {}
+
+    // Overload + operator to add two Complex objects
+    Complex operator + (const Complex& obj) {
+        Complex temp;
+        temp.real = real + obj.real;
+        temp.imag = imag + obj.imag;
+        return temp;
+    }
+
+    void display() {
+        std::cout << real << " + " << imag << "i" << std::endl;
+    }
+};
+
+int main() {
+    Complex c1(3.5, 2.5);
+    Complex c2(1.5, 4.5);
+    Complex c3;
+
+    c3 = c1 + c2;
+
+    std::cout << "Result of addition: ";
+    c3.display();
+
+    return 0;
+}
